4cfcfb371f3a875ac96496a98f72022a9aa7d659
Tomer Simis <tdl.simis@gmail.com>
2014-11-26 19:24:25 -0300

    Fixing camera movement

--- ProjetoPG.cpp
+++ ProjetoPG.cpp
@@ -14,6 +14,7 @@
 #include <algorithm>
 #include "Object.h"
 #include "Loader.h"
+#include "Camera.h"
 
 #define _CRT_SECURE_NO_WARNINGS
 
@@ -26,10 +27,7 @@
 
 std::vector<Object> objects;
 
-Point* lookAt;
-Point* camera;
-Vector cameraForward;
-Vector side;
+Camera camera;
 
 vector<vector<Point>> MEM_VEC_POINTS;
 vector<vector<Vector>> MEM_VEC_VECTOR;
@@ -53,7 +51,7 @@ void drawCoordinateSystem(){
 
 	glBegin(GL_LINES);
 
-	glVertex3d(-10000,0,0);
+	glVertex3d(-10000, 0, 0);
 	glVertex3d(10000, 0, 0);
 
 	glVertex3d(0, -10000, 0);
@@ -127,47 +125,29 @@ void display(){
 
 	glMatrixMode(GL_MODELVIEW);
 
-	/* View Matrix */
-	cameraForward = Vector(camera,lookAt);
-	cameraForward.normalize();
-
-	side = Vector::cross(cameraForward, Vector(0, 1, 0));
-	
-	Vector up = Vector::cross(cameraForward, side);
-	side.normalize();
-	up.normalize();
-
-		
-	double viewMatrix[16] = {
-		side.x, up.x, -cameraForward.x, 0,
-		side.y, up.y, -cameraForward.y, 0,
-		side.z, up.z, -cameraForward.z, 0,
-		0,0,0 , 1
+	double m[16] = {
+		camera.getMatrix()[0], camera.getMatrix()[1], camera.getMatrix()[2], camera.getMatrix()[3],
+		camera.getMatrix()[4], camera.getMatrix()[5], camera.getMatrix()[6], camera.getMatrix()[7],
+		camera.getMatrix()[8], camera.getMatrix()[9], camera.getMatrix()[10], camera.getMatrix()[11],
+		camera.getMatrix()[12], camera.getMatrix()[13], camera.getMatrix()[14], camera.getMatrix()[15],
 	};
 
-	
-
-	glLoadIdentity();
-	
-	glMultMatrixd(viewMatrix);
-
-	glTranslated(-camera->x, -camera->y, -camera->z);
-
+	glLoadMatrixd(m);
 
 	for (int i = 0; i < objects.size(); i++){
 		glPushMatrix();
 
 		translate(objects[i].translationX, objects[i].translationY, objects[i].translationZ);
 
-		
+
 		rotateX(objects[i].rotationX);
-		rotateY(objects[i].rotationY);	
+		rotateY(objects[i].rotationY);
 		rotateZ(objects[i].rotationZ);
 
 		scale(objects[i].scale);
-		
+
 		glBegin(GL_TRIANGLES);
-		
+
 		for (int j = 0; j < objects[i].faces.size(); j++){
 			//printf("\nChegou 2\n");
 #ifdef DRAW_NORMAL
@@ -184,7 +164,7 @@ void display(){
 
 	}
 
-	
+
 
 	/* Coordinate system drawing */
 	glPushMatrix();
@@ -224,278 +204,214 @@ void setLightning(){
 
 void reshape(int w, int h){
 
-	#ifdef DEBUG
-		printf("[DEBUG] Reshaping window, new size: %d x %d\n", w,h);
-	#endif
-		
-	glViewport(0,0,w,h);
+#ifdef DEBUG
+	printf("[DEBUG] Reshaping window, new size: %d x %d\n", w, h);
+#endif
+
+	glViewport(0, 0, w, h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glFrustum(-0.041421, 0.041421, -0.041421, 0.041421,near, 500);
+	glFrustum(-0.041421, 0.041421, -0.041421, 0.041421, near, 500);
 
 	setLightning();
 }
 
- void loadObj(std::string path){
-
-	 std::vector<Point*> vertex;
-	 std::vector<Face*> faces;
-	 std::vector<Vector*> normals;
-
-	std::ifstream file;
-	file.open(path);
-	std::string line;
-
-	int index = 0;
-
-	while (std::getline(file, line)){
-
-		char c[2];
-
-		double x, y, z;
-
-		file >> c;
-
-		if (c[0] != '#'){
-			file >> x >> y >> z;
-
-			if (c[0] == 'v' && c[1] == 'n'){
-				normals.push_back(new Vector(x, y, z));
-			}else if(c[0] == 'v'){
-				//factor = std::max(factor, std::max(x, std::max(y, z)));
-				vertex.push_back(new Point(x,y,z,index));
-				index++;
-			}else if(c[0] == 'f'){
-				faces.push_back(new Face(vertex[x - 1], vertex[y - 1], vertex[z - 1], new Vector(0,0,0)));
-			}else if (c[0] == 's'){
-
-			}
-		}
-
-	}
-
-	Object newObject = Object(vertex, faces, normals);
-	newObject.calculateNormals();
-	newObject.calculateVertexNormals();
-	objects.push_back(newObject);
-
-	file.close();
-
-	#ifdef DEBUG
-		printf("[DEBUG] Number of points loaded: %d\n", vertex.size());
-		printf("[DEBUG] Number of faces loaded: %d\n", faces.size());
-	#endif
-
-
-}
 
 char mode = 0; // 0 = obj, 1 = light
 void keyboard(unsigned char key, int x, int y){
 
 	switch (key){
 
-		case '1':
-			if (mode == 0){
-				objects[selectedObject].translationX -= 0.1;
-			}else{
-				light_position[light][0] += 0.05;
-				if (light_position[light][0] > 1) light_position[light][0] = 1;
-				if (light == 0){
-					glLightfv(GL_LIGHT0, GL_POSITION, light_position[light]);
-				}
-				else{
-					glLightfv(GL_LIGHT1, GL_POSITION, light_position[light]);
-				}
-			}
-			break;
-		case '2':
-			if (mode == 0){
-				objects[selectedObject].translationX += 0.1;
+	case '1':
+		if (mode == 0){
+			objects[selectedObject].translationX -= 0.1;
+		}
+		else{
+			light_position[light][0] += 0.05;
+			if (light_position[light][0] > 1) light_position[light][0] = 1;
+			if (light == 0){
+				glLightfv(GL_LIGHT0, GL_POSITION, light_position[light]);
 			}
 			else{
-				light_position[light][0] -= 0.05;
-				if (light_position[light][0] < -1) light_position[light][0] = -1;
-				if (light == 0){
-					glLightfv(GL_LIGHT0, GL_POSITION, light_position[light]);
-				}
-				else{
-					glLightfv(GL_LIGHT1, GL_POSITION, light_position[light]);
-				}
+				glLightfv(GL_LIGHT1, GL_POSITION, light_position[light]);
 			}
-			
-			break;
-		case '3':
-			if (mode == 0){
-				objects[selectedObject].translationY -= 0.1;
-			}
-			else{
-				light_position[light][1] += 0.05;
-				if (light_position[light][1] > 1) light_position[light][1] = 1;
-				if (light == 0){
-					glLightfv(GL_LIGHT0, GL_POSITION, light_position[light]);
-				}
-				else{
-					glLightfv(GL_LIGHT1, GL_POSITION, light_position[light]);
-				}
-			}
-			
-			break;
-		case '4':
-			if (mode == 0){
-				objects[selectedObject].translationY += 0.1;
+		}
+		break;
+	case '2':
+		if (mode == 0){
+			objects[selectedObject].translationX += 0.1;
+		}
+		else{
+			light_position[light][0] -= 0.05;
+			if (light_position[light][0] < -1) light_position[light][0] = -1;
+			if (light == 0){
+				glLightfv(GL_LIGHT0, GL_POSITION, light_position[light]);
 			}
 			else{
-				light_position[light][1] -= 0.05;
-				if (light_position[light][1] < -1) light_position[light][1] = -1;
-				if (light == 0){
-					glLightfv(GL_LIGHT0, GL_POSITION, light_position[light]);
-				}
-				else{
-					glLightfv(GL_LIGHT1, GL_POSITION, light_position[light]);
-				}
-			}			
-			break;
-		case '5':
-			if (mode == 0){
-				objects[selectedObject].translationZ -= 0.1;
+				glLightfv(GL_LIGHT1, GL_POSITION, light_position[light]);
+			}
+		}
+
+		break;
+	case '3':
+		if (mode == 0){
+			objects[selectedObject].translationY -= 0.1;
+		}
+		else{
+			light_position[light][1] += 0.05;
+			if (light_position[light][1] > 1) light_position[light][1] = 1;
+			if (light == 0){
+				glLightfv(GL_LIGHT0, GL_POSITION, light_position[light]);
 			}
 			else{
-				light_position[light][2] += 0.05;
-				if (light_position[light][2] > 1) light_position[light][2] = 1;
-				if (light == 0){
-					glLightfv(GL_LIGHT0, GL_POSITION, light_position[light]);
-				}
-				else{
-					glLightfv(GL_LIGHT1, GL_POSITION, light_position[light]);
-				}
+				glLightfv(GL_LIGHT1, GL_POSITION, light_position[light]);
 			}
-			
-			break;
-		case '6':
-			if (mode == 0){
-				objects[selectedObject].translationY += 0.1;
+		}
+
+		break;
+	case '4':
+		if (mode == 0){
+			objects[selectedObject].translationY += 0.1;
+		}
+		else{
+			light_position[light][1] -= 0.05;
+			if (light_position[light][1] < -1) light_position[light][1] = -1;
+			if (light == 0){
+				glLightfv(GL_LIGHT0, GL_POSITION, light_position[light]);
 			}
 			else{
-				light_position[light][2] -= 0.05;
-				if (light_position[light][2] < -1) light_position[light][2] = -1;
-				if (light == 0){
-					glLightfv(GL_LIGHT0, GL_POSITION, light_position[light]);
-				}
-				else{
-					glLightfv(GL_LIGHT1, GL_POSITION, light_position[light]);
-				}
+				glLightfv(GL_LIGHT1, GL_POSITION, light_position[light]);
 			}
-			
-			break;
-		case '7':
-			objects[selectedObject].rotationX += 0.1;
-			break;
-		case '8':
-			objects[selectedObject].rotationY += 0.1;
-			break;
-		case '9':
-			objects[selectedObject].rotationZ += 0.1;
-			break;
-		case 'w':
-		case 'W':
-			camera->z += cameraForward.z / 1;
-			camera->x += cameraForward.x / 1;
-			camera->y += cameraForward.y / 1;
-
-			lookAt->z += cameraForward.z / 1;
-			lookAt->x += cameraForward.x / 1;
-			lookAt->y += cameraForward.y / 1;
-			break;
-		case 's':
-		case 'S':
-			camera->z -= cameraForward.z / 1;
-			camera->x -= cameraForward.x / 1;
-			camera->y -= cameraForward.y / 1;
-
-			lookAt->z -= cameraForward.z / 1;
-			lookAt->x -= cameraForward.x / 1;
-			lookAt->y -= cameraForward.y / 1;
-			break;
-		case 'd':
-		case 'D':
-			camera->x += side.x/10;
-			camera->y += side.y/10;
-			camera->z += side.z/10;
-			break;
-		case 'a':
-		case 'A':
-			camera->x -= side.x / 10;
-			camera->y -= side.y / 10;
-			camera->z -= side.z / 10;
-			break;
-		case '-':
-		case '_':
-			objects[selectedObject].scale -= 0.1;
-			break;
-		case '=':
-		case '+':
-			objects[selectedObject].scale += 0.1;
-			break;
-		case 'n':
-		case 'N':
-			light = (light + 1) % 2;
-			break;
-		case 'M':
-		case 'm':
-			if (enable){
-				enable = false;
-				glDisable(GL_LIGHTING);
-			}else{
-				enable = true;
-				glEnable(GL_LIGHTING);
+		}
+		break;
+	case '5':
+		if (mode == 0){
+			objects[selectedObject].translationZ -= 0.1;
+		}
+		else{
+			light_position[light][2] += 0.05;
+			if (light_position[light][2] > 1) light_position[light][2] = 1;
+			if (light == 0){
+				glLightfv(GL_LIGHT0, GL_POSITION, light_position[light]);
 			}
-			break;
-		case '.':
-		case '>':
-			selectedObject = (selectedObject + 1) % objects.size();
-			break;
-		case ',':
-		case '<':
-			selectedObject -= 1;
-			if (selectedObject == -1) selectedObject = objects.size()-1;
-			break;
-		case 'b':
-		case 'B':
-			mode = (mode + 1) % 2;
-			break;
-		case 'z':
-		case 'Z':
-			camera->z += cameraForward.z / 3;
-			camera->x += cameraForward.x / 3;
-			camera->y += cameraForward.y / 3;
+			else{
+				glLightfv(GL_LIGHT1, GL_POSITION, light_position[light]);
+			}
+		}
 
-			lookAt->z += cameraForward.z / 3;
-			lookAt->x += cameraForward.x / 3;
-			lookAt->y += cameraForward.y / 3;
+		break;
+	case '6':
+		if (mode == 0){
+			objects[selectedObject].translationY += 0.1;
+		}
+		else{
+			light_position[light][2] -= 0.05;
+			if (light_position[light][2] < -1) light_position[light][2] = -1;
+			if (light == 0){
+				glLightfv(GL_LIGHT0, GL_POSITION, light_position[light]);
+			}
+			else{
+				glLightfv(GL_LIGHT1, GL_POSITION, light_position[light]);
+			}
+		}
 
-			near -= 0.0026;
+		break;
+	case '7':
+		objects[selectedObject].rotationX += 0.1;
+		break;
+	case '8':
+		objects[selectedObject].rotationY += 0.1;
+		break;
+	case '9':
+		objects[selectedObject].rotationZ += 0.1;
+		break;
+	case 'w':
+	case 'W':
+		camera.slide(0, 0, -1);
+		break;
+	case 's':
+	case 'S':
+		camera.slide(0, 0, 1);
+		break;
+	case 'd':
+	case 'D':
+		camera.slide(1, 0, 0);
+		break;
+	case 'a':
+	case 'A':
+		camera.slide(-1, 0, 0);
+		break;
+	case '-':
+	case '_':
+		objects[selectedObject].scale -= 0.1;
+		break;
+	case '=':
+	case '+':
+		objects[selectedObject].scale += 0.1;
+		break;
+	case 'n':
+	case 'N':
+		light = (light + 1) % 2;
+		break;
+	case 'M':
+	case 'm':
+		if (enable){
+			enable = false;
+			glDisable(GL_LIGHTING);
+		}
+		else{
+			enable = true;
+			glEnable(GL_LIGHTING);
+		}
+		break;
+	case '.':
+	case '>':
+		selectedObject = (selectedObject + 1) % objects.size();
+		break;
+	case ',':
+	case '<':
+		selectedObject -= 1;
+		if (selectedObject == -1) selectedObject = objects.size() - 1;
+		break;
+	case 'b':
+	case 'B':
+		mode = (mode + 1) % 2;
+		break;
+	case 'z':
+	case 'Z':
+		camera.slide(0, 0, -0.2);
+		//camera->z += cameraForward.z / 3;
+		//camera->x += cameraForward.x / 3;
+		//camera->y += cameraForward.y / 3;
 
-			glMatrixMode(GL_PROJECTION);
-			glLoadIdentity();
-			glFrustum(-0.041421, 0.041421, -0.041421, 0.041421, near, 500);
-			break;
-		case 'x':
-		case 'X':
-			camera->z -= cameraForward.z / 3;
-			camera->x -= cameraForward.x / 3;
-			camera->y -= cameraForward.y / 3;
+		//lookAt->z += cameraForward.z / 3;
+		//lookAt->x += cameraForward.x / 3;
+		//lookAt->y += cameraForward.y / 3;
 
-			lookAt->z -= cameraForward.z / 3;
-			lookAt->x -= cameraForward.x / 3;
-			lookAt->y -= cameraForward.y / 3;
+		near -= 0.0026;
 
-			near += 0.0026;
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glFrustum(-0.041421, 0.041421, -0.041421, 0.041421, near, 500);
+		break;
+	case 'x':
+	case 'X':
+		camera.slide(0, 0, 0.2);
+		//camera->z -= cameraForward.z / 3;
+		//camera->x -= cameraForward.x / 3;
+		//camera->y -= cameraForward.y / 3;
 
-			glMatrixMode(GL_PROJECTION);
-			glLoadIdentity();
-			glFrustum(-0.041421, 0.041421, -0.041421, 0.041421, near, 500);
-			break;
-			break;
+		//lookAt->z -= cameraForward.z / 3;
+		//lookAt->x -= cameraForward.x / 3;
+		//lookAt->y -= cameraForward.y / 3;
+
+		near += 0.0026;
+
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glFrustum(-0.041421, 0.041421, -0.041421, 0.041421, near, 500);
+		break;
+		break;
 
 	}
 
@@ -506,15 +422,15 @@ void mainMenu(int item){
 	std::vector<Point*> vertex;
 	std::vector<Face*> faces;
 	std::vector<Vector*> normals;
-	string s="";
+	string s = "";
 	if (item == 0){
 		//Loader("Obj\\cubo1.obj", vertex, normals, faces);
-	
+
 	}
 	else if (item == 1){
 		s = "Obj\\pumpkin.obj";
 	}
-	
+
 	else if (item == 2){
 		s = "Obj\\apple.obj";
 	}
@@ -546,7 +462,7 @@ void mainMenu(int item){
 	MEM_VEC_FACES.push_back(CONST_f);
 
 	//Loader l = Loader(&s, &CONST_P, &CONST_vec, &CONST_f);
-	Loader l = Loader(&s, &MEM_VEC_POINTS[MEM_VEC_POINTS.size() - 1], &MEM_VEC_VECTOR[MEM_VEC_VECTOR.size() - 1], &MEM_VEC_FACES[MEM_VEC_FACES.size()-1]);
+	Loader l = Loader(&s, &MEM_VEC_POINTS[MEM_VEC_POINTS.size() - 1], &MEM_VEC_VECTOR[MEM_VEC_VECTOR.size() - 1], &MEM_VEC_FACES[MEM_VEC_FACES.size() - 1]);
 	l.load();
 	for (int i = 0; i < MEM_VEC_POINTS[MEM_VEC_POINTS.size() - 1].size(); i++){
 		vertex.push_back(&MEM_VEC_POINTS[MEM_VEC_POINTS.size() - 1][i]);
@@ -576,17 +492,18 @@ void mousePress(int btn, int state, int x, int y){
 	if (btn == GLUT_LEFT_BUTTON){
 		if (state == GLUT_UP){
 			drag = false;
-		}else{
+		}
+		else{
 			drag = true;
 			mouseInitialPosition->x = x;
 			mouseInitialPosition->y = y;
-	    }
+		}
 	}
 }
 
 void mouseMove(int x, int y){
-	lookAt->x += (x - mouseInitialPosition->x)*0.01;
-	lookAt->y += (y - mouseInitialPosition->y)*0.01;
+	camera.yawAngle += (x - mouseInitialPosition->x)*0.5;
+	camera.pitchAngle += (y - mouseInitialPosition->y)*0.5;
 	mouseInitialPosition->x = x;
 	mouseInitialPosition->y = y;
 }
@@ -594,7 +511,7 @@ void mouseMove(int x, int y){
 int main(int argc, char** argv)
 {
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-	
+
 	mainMenu(1);
 	mainMenu(4);
 
@@ -613,8 +530,8 @@ int main(int argc, char** argv)
 
 	mousePosition = new Point(0, 0, 0);
 	mouseInitialPosition = new Point(0, 0, 0);
-	lookAt = new Point(0, 0, 0);
-	camera = new Point(0, 0, 10);
+
+	camera = Camera(0, 0, 100);
 
 	glutMainLoop();
 	return 0;
